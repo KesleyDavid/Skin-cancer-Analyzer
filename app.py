77ebc2477cae1fefe3e988cde158dc660892864c
Kesley David <39314443+KesleyDavid@users.noreply.github.com>
2020-12-11 17:47:49 +0000

    [test] Change kes

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ DATAPATH = './data/'
 def render_header():
     st.write("""
         <p align="center"> 
-            <H1> Skin cancer Analyzer 
+            <H1> Skin cancer Analyzer [kes]
         </p>
 
     """, unsafe_allow_html=True)
